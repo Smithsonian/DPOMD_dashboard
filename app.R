09c007277327d6006e5618f882bb399989dd13ab
Luis J. Villanueva <villanueval@si.edu>
2020-03-03 10:58:26 -0500

    Removing database functions, data from RData file

--- app.R
+++ app.R
@@ -312,7 +312,7 @@ server <- function(input, output, session) {
       project_end <- ""
     }
     
-    proj_media <- dbGetQuery(db, paste0("SELECT * FROM projects_media WHERE project_id = ", proj_info$project_id))
+    proj_media <- projects_media[projects_media$project_id == proj_info$project_id, ]
     
     media_links <- ""
     
@@ -328,7 +328,8 @@ server <- function(input, output, session) {
       media_links <- paste0(media_links, "<hr>")
     }
     
-    unit <- projects_units[projects_units$project_unit == proj_info$project_unit, ]
+    print(projects_units)
+    unit <- projects_units[projects_units$unit == proj_info$project_unit, ]
     
     showModal(modalDialog(
       size = "l",
@@ -357,197 +358,197 @@ server <- function(input, output, session) {
   output$edan <- renderUI({
     req(FALSE)
     #Rewrite, too slow
-    req(input$projects_rows_selected)
-    
-    selected_project_id <- projects_list[input$projects_rows_selected,]$project_id
-    
-    query_edan <- dbGetQuery(db, paste0("SELECT * from projects_edan WHERE project_id = ", selected_project_id))
-    
-    if (dim(query_edan)[1] > 0){
-      if (dim(query_edan)[1] > 2){
-        edan_examples <<- dplyr::sample_n(query_edan, 2, replace = FALSE)
-      }else{
-        edan_examples <<- query_edan
-      }
-    }else{
-      req(FALSE)
-    }
-    
-    this_item_search <- EDANr::searchEDAN(q = edan_examples[1,]$edan_id, AppID = AppID, AppKey = AppKey)
-    if (this_item_search$rowCount == 1){
-      edan_id <- this_item_search$rows$url
-      
-      this_item <- EDANr::getContentEDAN(itemID = edan_id, AppID = AppID, AppKey = AppKey)
-      
-      IDSid1 <- this_item$content$descriptiveNonRepeating$online_media$media$idsId[1]
-      if (!is.null(IDSid1)){
-        title1 <- this_item$content$descriptiveNonRepeating$title$content[1]
-        if (title1 == "Untitled Object"){
-          title1 <- ""
-        }
-        
-        link1 <- this_item$content$descriptiveNonRepeating$record_link[1]
-        if (is.null(link1)){
-          link1 <- paste0("http://collections.si.edu/search/detail/", edan_id)
-        }
-        
-        credit1 <- this_item$content$freetext$creditLine$content[1]
-        if (!is.null(credit1)){
-          credit1 <- paste0("<br>", credit1)
-        }else{
-          credit1 <- ""
-        }
-        
-        notes1 <- paste(this_item$content$freetext$notes$content[1], collapse = ". ")
-        if (!is.null(notes1) && notes1 != "1"){
-          notes1 <- paste0("<br>", notes1)
-        }else{
-          notes1 <- ""
-        }
-        
-        if (substr(IDSid1, 0, 4) != 'http'){
-          IDSid1 <- paste0('http://ids.si.edu/ids/deliveryService?id=', IDSid1)
-        }
-        
-        edan1_img <- HTML(paste0(paste0("<h4>Example images from the project:</h4><a href=\"", IDSid1, "\" target = _blank><img class = \"loading\" src=\"", IDSid1, "&max_h=130\" style=\"max-height: 130px; width:auto;\"></a>", 
-                                        "<dl><dd>", title1, 
-                                        credit1, 
-                                        notes1, 
-                                        "<br><a href=\"", link1, "\" target = _blank>Object record</a></dd></dl>")))
-      }else{
-        edan1_img <- ""
-      }
-    }else{
-      if (edan_examples[1,]$dams_only == '1'){
-        edan1_img <- HTML(paste0(paste0("<h4>Example images from the project:</h4><a href=\"http://ids.si.edu/ids/dynamic?id=", 
-                                        edan_examples[1,]$edan_id, 
-                                        "\" target = _blank><img class = \"loading\" src=\"http://ids.si.edu/ids/deliveryService?id=", 
-                                        edan_examples[1,]$edan_id, 
-                                        "&max_h=130\" style=\"max-height: 130px; width:auto;\"></a><p><em>Details and link not available</em></p><br>")))
-      }else{
-        #Try using the id directly
-        this_item <- EDANr::getContentEDAN(itemID = edan_examples[1,]$edan_id, AppID = AppID, AppKey = AppKey)
-        
-        IDSid1 <- this_item$content$descriptiveNonRepeating$online_media$media$idsId[1]
-        if (!is.null(IDSid1)){
-          title1 <- this_item$content$descriptiveNonRepeating$title$content[1]
-          if (title1 == "Untitled Object"){
-            title1 <- ""
-          }
-          
-          link1 <- this_item$content$descriptiveNonRepeating$record_link[1]
-          if (is.null(link1)){
-            link1 <- paste0("http://collections.si.edu/search/detail/", edan_id)
-          }
-          
-          credit1 <- this_item$content$freetext$creditLine$content[1]
-          if (!is.null(credit1)){
-            credit1 <- paste0("<br>", credit1)
-          }else{
-            credit1 <- ""
-          }
-          
-          notes1 <- paste(this_item$content$freetext$notes$content[1], collapse = ". ")
-          if (!is.null(notes1) && notes1 != "1"){
-            notes1 <- paste0("<br>", notes1)
-          }else{
-            notes1 <- ""
-          }
-          
-          if (substr(IDSid1, 0, 4) != 'http'){
-            IDSid1 <- paste0('http://ids.si.edu/ids/deliveryService?id=', IDSid1)
-          }
-          
-          edan1_img <- HTML(paste0(paste0("<h4>Example images from the project:</h4><a href=\"", IDSid1, "\" target = _blank><img class = \"loading\" src=\"", IDSid1, "&max_h=130\" style=\"max-height: 130px; width:auto;\"></a>", 
-                                          "<dl><dd>", title1, 
-                                          credit1, 
-                                          notes1, 
-                                          "<br><a href=\"", link1, "\" target = _blank>Link</a></dd></dl>")))
-        }else{
-          edan1_img <- ""
-        }
-      }
-    }
-
-    if (dim(edan_examples)[1] == 2){
-        
-      this_item_search <- EDANr::searchEDAN(q = edan_examples[2,]$edan_id, AppID = AppID, AppKey = AppKey)
-      if (this_item_search$rowCount == 1){
-        edan_id <- this_item_search$rows$url
-        
-        this_item <- EDANr::getContentEDAN(itemID = edan_id, AppID = AppID, AppKey = AppKey)
-        
-        IDSid2 <- this_item$content$descriptiveNonRepeating$online_media$media$idsId[1]
-        if (!is.null(IDSid2)){
-            
-          title2 <- this_item$content$descriptiveNonRepeating$title$content[1]
-          if (title2 == "Untitled Object"){
-            title2 <- ""
-          }
-          
-          link2 <- this_item$content$descriptiveNonRepeating$record_link[1]
-          if (is.null(link2)){
-            link2 <- paste0("http://collections.si.edu/search/detail/", edan_id)
-          }
-          credit2 <- this_item$content$freetext$creditLine$content[1]
-          if (!is.null(credit2)){
-            credit2 <- paste0("<br>", credit2)
-          }else{
-            credit2 <- ""
-          }
-          
-          notes2 <- paste(this_item$content$freetext$notes$content[1], collapse = ". ")
-          if (!is.null(notes2) && notes2 != "1"){
-            notes2 <- paste0("<br>", notes2)
-          }else{
-            notes2 <- ""
-          }
-          
-          if (substr(IDSid2, 0, 4) != 'http'){
-            IDSid2 <- paste0('http://ids.si.edu/ids/deliveryService?id=', IDSid2)
-          }
-          
-          edan2_img <- HTML(paste0(paste0("<p>&nbsp;</p><a href=\"", IDSid2, "\" target = _blank><img class = \"loading\" src=\"", IDSid2, "&max_h=130\" style=\"max-height: 130px; width:auto;\"></a>", 
-                                        "<dl><dd>", title2, 
-                                        credit2, 
-                                        notes2, 
-                                        "<br><a href=\"", link2, "\" target = _blank>Object record</a></dd></dl>")))
-        }else{
-          edan2_img <- ""
-          cat(paste0("error", edan_examples[2,]$edan_id))
-        }
-        
-      }else{
-        if (edan_examples[2,]$dams_only == '1'){
-          edan2_img <- HTML(paste0(paste0("<p>&nbsp;</p><a href=\"http://ids.si.edu/ids/dynamic?id=", 
-                                        edan_examples[2,]$edan_id, 
-                                        "\" target = _blank><img class = \"loading\" src=\"http://ids.si.edu/ids/deliveryService?id=", 
-                                        edan_examples[2,]$edan_id, 
-                                        "&max_h=130\" style=\"max-height: 130px; width:auto;\"></a><p><em>Details and link not available</em></p><br>")))
-        }
-      }
-      
-    }else{
-      edan2_img <- ""
-    }
-    
-    if (edan1_img != "" && edan2_img != ""){
-      edan_hr <- "<hr>"
-    }else{
-      edan_hr <- ""
-    }
-    
-    tagList(
-      fluidRow(
-        column(width = 6,
-               edan1_img
-        ),
-        column(width = 6,
-               edan2_img
-        )
-      ),
-      HTML(edan_hr)
-    )
+    # req(input$projects_rows_selected)
+    # 
+    # selected_project_id <- projects_list[input$projects_rows_selected,]$project_id
+    # 
+    # query_edan <- dbGetQuery(db, paste0("SELECT * from projects_edan WHERE project_id = ", selected_project_id))
+    # 
+    # if (dim(query_edan)[1] > 0){
+    #   if (dim(query_edan)[1] > 2){
+    #     edan_examples <<- dplyr::sample_n(query_edan, 2, replace = FALSE)
+    #   }else{
+    #     edan_examples <<- query_edan
+    #   }
+    # }else{
+    #   req(FALSE)
+    # }
+    # 
+    # this_item_search <- EDANr::searchEDAN(q = edan_examples[1,]$edan_id, AppID = AppID, AppKey = AppKey)
+    # if (this_item_search$rowCount == 1){
+    #   edan_id <- this_item_search$rows$url
+    #   
+    #   this_item <- EDANr::getContentEDAN(itemID = edan_id, AppID = AppID, AppKey = AppKey)
+    #   
+    #   IDSid1 <- this_item$content$descriptiveNonRepeating$online_media$media$idsId[1]
+    #   if (!is.null(IDSid1)){
+    #     title1 <- this_item$content$descriptiveNonRepeating$title$content[1]
+    #     if (title1 == "Untitled Object"){
+    #       title1 <- ""
+    #     }
+    #     
+    #     link1 <- this_item$content$descriptiveNonRepeating$record_link[1]
+    #     if (is.null(link1)){
+    #       link1 <- paste0("http://collections.si.edu/search/detail/", edan_id)
+    #     }
+    #     
+    #     credit1 <- this_item$content$freetext$creditLine$content[1]
+    #     if (!is.null(credit1)){
+    #       credit1 <- paste0("<br>", credit1)
+    #     }else{
+    #       credit1 <- ""
+    #     }
+    #     
+    #     notes1 <- paste(this_item$content$freetext$notes$content[1], collapse = ". ")
+    #     if (!is.null(notes1) && notes1 != "1"){
+    #       notes1 <- paste0("<br>", notes1)
+    #     }else{
+    #       notes1 <- ""
+    #     }
+    #     
+    #     if (substr(IDSid1, 0, 4) != 'http'){
+    #       IDSid1 <- paste0('http://ids.si.edu/ids/deliveryService?id=', IDSid1)
+    #     }
+    #     
+    #     edan1_img <- HTML(paste0(paste0("<h4>Example images from the project:</h4><a href=\"", IDSid1, "\" target = _blank><img class = \"loading\" src=\"", IDSid1, "&max_h=130\" style=\"max-height: 130px; width:auto;\"></a>", 
+    #                                     "<dl><dd>", title1, 
+    #                                     credit1, 
+    #                                     notes1, 
+    #                                     "<br><a href=\"", link1, "\" target = _blank>Object record</a></dd></dl>")))
+    #   }else{
+    #     edan1_img <- ""
+    #   }
+    # }else{
+    #   if (edan_examples[1,]$dams_only == '1'){
+    #     edan1_img <- HTML(paste0(paste0("<h4>Example images from the project:</h4><a href=\"http://ids.si.edu/ids/dynamic?id=", 
+    #                                     edan_examples[1,]$edan_id, 
+    #                                     "\" target = _blank><img class = \"loading\" src=\"http://ids.si.edu/ids/deliveryService?id=", 
+    #                                     edan_examples[1,]$edan_id, 
+    #                                     "&max_h=130\" style=\"max-height: 130px; width:auto;\"></a><p><em>Details and link not available</em></p><br>")))
+    #   }else{
+    #     #Try using the id directly
+    #     this_item <- EDANr::getContentEDAN(itemID = edan_examples[1,]$edan_id, AppID = AppID, AppKey = AppKey)
+    #     
+    #     IDSid1 <- this_item$content$descriptiveNonRepeating$online_media$media$idsId[1]
+    #     if (!is.null(IDSid1)){
+    #       title1 <- this_item$content$descriptiveNonRepeating$title$content[1]
+    #       if (title1 == "Untitled Object"){
+    #         title1 <- ""
+    #       }
+    #       
+    #       link1 <- this_item$content$descriptiveNonRepeating$record_link[1]
+    #       if (is.null(link1)){
+    #         link1 <- paste0("http://collections.si.edu/search/detail/", edan_id)
+    #       }
+    #       
+    #       credit1 <- this_item$content$freetext$creditLine$content[1]
+    #       if (!is.null(credit1)){
+    #         credit1 <- paste0("<br>", credit1)
+    #       }else{
+    #         credit1 <- ""
+    #       }
+    #       
+    #       notes1 <- paste(this_item$content$freetext$notes$content[1], collapse = ". ")
+    #       if (!is.null(notes1) && notes1 != "1"){
+    #         notes1 <- paste0("<br>", notes1)
+    #       }else{
+    #         notes1 <- ""
+    #       }
+    #       
+    #       if (substr(IDSid1, 0, 4) != 'http'){
+    #         IDSid1 <- paste0('http://ids.si.edu/ids/deliveryService?id=', IDSid1)
+    #       }
+    #       
+    #       edan1_img <- HTML(paste0(paste0("<h4>Example images from the project:</h4><a href=\"", IDSid1, "\" target = _blank><img class = \"loading\" src=\"", IDSid1, "&max_h=130\" style=\"max-height: 130px; width:auto;\"></a>", 
+    #                                       "<dl><dd>", title1, 
+    #                                       credit1, 
+    #                                       notes1, 
+    #                                       "<br><a href=\"", link1, "\" target = _blank>Link</a></dd></dl>")))
+    #     }else{
+    #       edan1_img <- ""
+    #     }
+    #   }
+    # }
+    # 
+    # if (dim(edan_examples)[1] == 2){
+    #     
+    #   this_item_search <- EDANr::searchEDAN(q = edan_examples[2,]$edan_id, AppID = AppID, AppKey = AppKey)
+    #   if (this_item_search$rowCount == 1){
+    #     edan_id <- this_item_search$rows$url
+    #     
+    #     this_item <- EDANr::getContentEDAN(itemID = edan_id, AppID = AppID, AppKey = AppKey)
+    #     
+    #     IDSid2 <- this_item$content$descriptiveNonRepeating$online_media$media$idsId[1]
+    #     if (!is.null(IDSid2)){
+    #         
+    #       title2 <- this_item$content$descriptiveNonRepeating$title$content[1]
+    #       if (title2 == "Untitled Object"){
+    #         title2 <- ""
+    #       }
+    #       
+    #       link2 <- this_item$content$descriptiveNonRepeating$record_link[1]
+    #       if (is.null(link2)){
+    #         link2 <- paste0("http://collections.si.edu/search/detail/", edan_id)
+    #       }
+    #       credit2 <- this_item$content$freetext$creditLine$content[1]
+    #       if (!is.null(credit2)){
+    #         credit2 <- paste0("<br>", credit2)
+    #       }else{
+    #         credit2 <- ""
+    #       }
+    #       
+    #       notes2 <- paste(this_item$content$freetext$notes$content[1], collapse = ". ")
+    #       if (!is.null(notes2) && notes2 != "1"){
+    #         notes2 <- paste0("<br>", notes2)
+    #       }else{
+    #         notes2 <- ""
+    #       }
+    #       
+    #       if (substr(IDSid2, 0, 4) != 'http'){
+    #         IDSid2 <- paste0('http://ids.si.edu/ids/deliveryService?id=', IDSid2)
+    #       }
+    #       
+    #       edan2_img <- HTML(paste0(paste0("<p>&nbsp;</p><a href=\"", IDSid2, "\" target = _blank><img class = \"loading\" src=\"", IDSid2, "&max_h=130\" style=\"max-height: 130px; width:auto;\"></a>", 
+    #                                     "<dl><dd>", title2, 
+    #                                     credit2, 
+    #                                     notes2, 
+    #                                     "<br><a href=\"", link2, "\" target = _blank>Object record</a></dd></dl>")))
+    #     }else{
+    #       edan2_img <- ""
+    #       cat(paste0("error", edan_examples[2,]$edan_id))
+    #     }
+    #     
+    #   }else{
+    #     if (edan_examples[2,]$dams_only == '1'){
+    #       edan2_img <- HTML(paste0(paste0("<p>&nbsp;</p><a href=\"http://ids.si.edu/ids/dynamic?id=", 
+    #                                     edan_examples[2,]$edan_id, 
+    #                                     "\" target = _blank><img class = \"loading\" src=\"http://ids.si.edu/ids/deliveryService?id=", 
+    #                                     edan_examples[2,]$edan_id, 
+    #                                     "&max_h=130\" style=\"max-height: 130px; width:auto;\"></a><p><em>Details and link not available</em></p><br>")))
+    #     }
+    #   }
+    #   
+    # }else{
+    #   edan2_img <- ""
+    # }
+    # 
+    # if (edan1_img != "" && edan2_img != ""){
+    #   edan_hr <- "<hr>"
+    # }else{
+    #   edan_hr <- ""
+    # }
+    # 
+    # tagList(
+    #   fluidRow(
+    #     column(width = 6,
+    #            edan1_img
+    #     ),
+    #     column(width = 6,
+    #            edan2_img
+    #     )
+    #   ),
+    #   HTML(edan_hr)
+    # )
   })
   
 
